553505d61f3261e8b69d2d1070960b69df401ab2
BlueProgrammer212 <prog46357@gmail.com>
2023-01-30 23:30:21 +0800

    Creating a better UI-friendly environment for mobile users.

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 'use strict'
 
-const fade_up = document.querySelectorAll(".fade-up");
-const slide = document.querySelectorAll(".slide-right");
-const fade_in = document.querySelectorAll(".fade-in");
-
 const fonts = document.querySelectorAll("[data-font-size]");
 
-window.addEventListener("load", (env) => {
-    setTimeout(() => {
-        fade_up.forEach(e => e.classList.add("loaded-fade-up"));
-        slide.forEach(e => e.classList.add("loaded-slide"));
-        fade_in.forEach(e => e.classList.add("loaded-fade-in"));
-    }, 400);
-})
+class Transition {
+  constructor() {
+     const config = {}; //Configure intersection observer.
+     this.observer = new IntersectionObserver();
+  }
+}
+
+window.addEventListener("offline",
+    (e) => console.log(`Failed to load resources 
+    due to poor internet connectivity.`))
+
+function addClass(name) {
+  for (let i = 0; i < name.length; ++i) {
+    const element = document.querySelectorAll(`.${name[i]}`);
+    element.forEach(e => e.classList.add(`loaded-${name[i]}`));
+  }
+}
+
+function loaded(env) {
+  setTimeout(addClass, 400, ["fade-in", "fade-up", "slide"]);
+}
+
+window.addEventListener("load", loaded);
 
 for (let i = 0, l = document.images.length; i < l; ++i) {
     document.images[i].setAttribute("draggable", "false");
@@ -20,5 +32,5 @@ for (let i = 0, l = document.images.length; i < l; ++i) {
 
 for (let i = 0, l = fonts.length; i < l; ++i) {
     let data = fonts[i].getAttribute("data-font-size");
-    fonts[i].style = `font-size: ${data}px`;
+    fonts[i].setAttribute("style", `font-size: ${data}px;`);
 }
